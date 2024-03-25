ddd05fefa7cc125443de51cb5dbefa8b32d8eef9
din <din@pnzgu.ru>
2024-03-25 15:10:54 +0300

    исправлена модалка

--- dzudo-client/src/views/UIpack/Auth/Auth.jsx
+++ dzudo-client/src/views/UIpack/Auth/Auth.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 // import { Link } from "react-router-dom";
 import { setUser } from "../../../store/slices/userSlice";
-import { Button, Box, Grid, TextField, Modal, Card, CardHeader, CardContent, CardActions, Fade } from "@mui/material";
+import { Button, Box, Grid, TextField, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Card, CardHeader, CardContent, CardActions, Fade } from "@mui/material";
 import { Form } from "react-router-dom";
 
 // import axios from "axios";
@@ -11,117 +11,56 @@ import { login } from "../../../core/Api/ApiAuth/methodsAuth";
 import "./Auth.css"
 // import InputWithImage from "../Input/InputWithImage";
 import toast from "react-hot-toast";
-import { AccountCircle } from "@material-ui/icons";
+import AccountCircle from '@mui/icons-material/AccountCircle';
 
 function AuthModal({headerSendForm, handleClose, ...props}) {
-    console.debug('modal');
+    // console.debug(props, headerSendForm, handleClose);
     return (
-        <Modal
-            // {...props}
-            // size="lg"
-            open={props.open}
-            onClose={props.handleClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-            // centered
+        <Dialog
+          open={props.open}
+          keepMounted
+          onClose={handleClose}
+          aria-describedby="alert-dialog-slide-description"
         >
-        <Fade in={props.open}>
-            
-            {/* <Typography id="modal-modal-title" variant="h6" component="h2">
-                Авторизация на сайте
-            </Typography>
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-            </Typography> */}
-            <Card>
-                <CardHeader closeButton id="modal-modal-title">
-                    Авторизация на сайте
-                </CardHeader>
-                <CardContent id="modal-modal-description">
-                    <Form controlId="authform">
-                        <Grid>
-                            <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-                                <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-                                <TextField 
-                                    id="input-with-sx" 
-                                    label="Логин" 
-                                    name="login" 
-                                    autoFocus 
-                                    variant="standard" 
-                                    />
-                            </Box>
-                            <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-                                <TextField 
-                                    id="input-with-sx" 
-                                    label="Пароль" 
-                                    type="password" 
-                                    name="password" 
-                                    variant="standard" 
-                                    />
-                            </Box>
-                        </Grid>
-                    </Form>
-                </CardContent>
-                <CardActions>
-                    <Grid>
-                        <Box item>
-                            <Button  variant="outlined">
-                                ЗАБЫЛИ ПАРОЛЬ?
-                            </Button>
-                            <Button  variant="outlined" type="submit" for="authform" onClick={headerSendForm}>
-                                ВОЙТИ
-                            </Button>
-                        </Box>
-                    </Grid>
-                </CardActions>
-            </Card>
-            {/* <Modal.Header>
-                <Modal.Title id="contained-modal-title-vcenter">
-                    Авторизация на сайте
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Form controlId="authform">
-                    <Grid>
-                        <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-                            <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-                            <TextField 
-                                id="input-with-sx" 
-                                label="Логин" 
-                                name="login" 
-                                autoFocus 
-                                variant="standard" 
-                                 />
-                        </Box>
-                        <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-                            <TextField 
-                                id="input-with-sx" 
-                                label="Пароль" 
-                                type="password" 
-                                name="password" 
-                                variant="standard" 
-                                 />
-                        </Box>
-                    </Grid>
-                </Form>
-            </Modal.Body>
-            <Modal.Footer> */}
-                {/* <Button variant="secondary">
-                    РЕГИСТРАЦИЯ АБИТУРИЕНТА
-                </Button> */}
-                {/* <Grid>
-                    <Box item>
-                        <Button  variant="outlined">
-                            ЗАБЫЛИ ПАРОЛЬ?
-                        </Button>
-                        <Button  variant="outlined" type="submit" for="authform" onClick={headerSendForm}>
-                            ВОЙТИ
-                        </Button>
-                    </Box>
-                </Grid>
-            </Modal.Footer> */}
-        </Fade>
-        </Modal>
+            <DialogTitle>
+                Авторизация
+            </DialogTitle>
+            <DialogContent>
+                <DialogContentText id="alert-dialog-slide-description">
+                        <Form id="authform">
+                            <Grid>
+                                <Box my={1} sx={{ display: 'flex', alignItems: 'flex-end', justifyContent: 'flex-end' }}>
+                                    <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+                                    <TextField 
+                                        id="input-with-sx" 
+                                        label="Логин" 
+                                        name="login" 
+                                        autoFocus 
+                                        variant="standard" 
+                                        />
+                                </Box>
+                                <Box my={1} sx={{ display: 'flex', alignItems: 'flex-end', justifyContent: 'flex-end' }}>
+                                    <TextField 
+                                        id="input-with-sx" 
+                                        label="Пароль" 
+                                        type="password" 
+                                        name="password" 
+                                        variant="standard" 
+                                        />
+                                </Box>
+                            </Grid>
+                        </Form>
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button  variant="outlined">
+                    ЗАБЫЛИ ПАРОЛЬ?
+                </Button>
+                <Button  variant="outlined" type="submit" for="authform" onClick={headerSendForm}>
+                    ВОЙТИ
+                </Button>
+            </DialogActions>
+        </Dialog>
     )
 }
 function ButtonAuth(props) {
@@ -142,13 +81,6 @@ function Auth() {
         dataLogin[event.target.name] = event.target.value;
         setDataLogin(dataLogin)
     }
-    // axios.post("https://lk.pnzgu.ru/ajax/cloud/auth", {login:'s517670535', password: 'p12rqa20SD'}, {
-    //     withCredentials: true,
-    //   });
-
-    //   axios.post("https://lk.pnzgu.ru/ajax/api", {login:'s517670535', password: 'p12rqa20SD'}, {
-    //     withCredentials: true,
-    //   });
 
     const dispatch = useDispatch();
     const shortName = useSelector((state) => state.user.shortName);
@@ -178,7 +110,7 @@ function Auth() {
             <AuthModal
                 headerSendForm={sendform}
                 open={modalShow}
-                handleClose={() => handleClose}
+                handleClose={handleClose}
                 onChange={handleOnChangeLoginForm}
             />
 
