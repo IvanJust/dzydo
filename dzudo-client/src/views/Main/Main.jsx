7bf6c0df7b64928820a1f92af15482b61634ef07
din <din@pnzgu.ru>
2024-05-02 14:48:22 +0300

    clear

--- dzudo-client/src/views/Main/Main.jsx
+++ dzudo-client/src/views/Main/Main.jsx
@@ -7,7 +7,7 @@ import fight from '../../images/main.gif';
 
 export default function Main(){
     return(
-        <Grid my={1} container justifyContent={'center'}>
+        <Grid my={1} justifyContent={'center'} flexDirection={'column'}>
             <Typography fontSize={24}>
                 Приветсвуем вас на сайте соревнований Дзюдо Ката
             </Typography>
